307bbc26b4b34c3a258487ec620b30f7d01e5aff
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-08-28 03:40:15 +0000

    Refactor Flask app to use separate templates for each language

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,10 +4,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     """the index route"""
-    return render_template("index.html")
+    return render_template("0-index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -3,25 +3,30 @@
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
+
 class Config:
     """Config class for Flask app settings."""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 def get_locale():
     """Get the best match locale from the request."""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
 babel = Babel(app, locale_selector=get_locale)
 
+
 @app.route("/")
 def index():
     """The index route."""
     return render_template("1-index.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
