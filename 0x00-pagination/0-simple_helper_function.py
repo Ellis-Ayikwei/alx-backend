0fe27a4ef5e06b9f8bc74292b99d0a11115b6585
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-03 08:39:37 +0000

    Refactor get_locale function to include user locale from user settings

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Defines function named index_range that takes two
+integer arguments page and page_size
+"""
+
+def index_range(page: int, page_size: int) -> (int, int):
+    """Returns the endpage and the startpage of
+    
+    Keyword arguments:
+    page -- the page
+    page_size -- the size of the page
+    Return: the endpage idex and the startpage index
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
