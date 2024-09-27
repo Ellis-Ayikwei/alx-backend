b0cd41b8f73f7aeabe4518cbc0af7c1f22fe43ee
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-27 11:51:35 +0000

    remove the set functionality that was assigning the data to the key functionality

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -12,12 +12,10 @@ class FIFOCache(BaseCaching):
     def put(self, key: str, item: str) -> None:
         """Adds an item to the cache if both key and item are not None"""
         if key is not None and item is not None:  # Check both key and item
-            if len(self.cache_data) > self.MAX_ITEMS:
-                keys = list(self.cache_data.keys())
-                first_key = keys[0]
+             if len(self.cache_data) >= self.MAX_ITEMS:
+                first_key = list(self.cache_data.keys())[0]
                 del self.cache_data[first_key]
                 print(f"DISCARD: {first_key}")
-            self.cache_data[key] = item
 
     def get(self, key: str) -> str:
         """Retrieves an item from the cache"""
