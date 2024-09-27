1be14fb0bf4c569ccc533369fd95b4822526a424
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-27 11:59:13 +0000

    the put functionality

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -12,11 +12,13 @@ class FIFOCache(BaseCaching):
     def put(self, key: str, item: str) -> None:
         """Adds an item to the cache if both key and item are not None"""
         if key is not None and item is not None:  # Check both key and item
-             if len(self.cache_data) >= self.MAX_ITEMS:
+            if len(self.cache_data) >= self.MAX_ITEMS:
                 first_key = list(self.cache_data.keys())[0]
                 del self.cache_data[first_key]
                 print(f"DISCARD: {first_key}")
 
+                self.cache_data[key] = item
+            self.cache_data[key] = item
     def get(self, key: str) -> str:
         """Retrieves an item from the cache"""
         if key is None:
