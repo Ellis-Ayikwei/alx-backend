7500f2edb98c8dbb2c33a1f3b3ef5e9a391fe200
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-27 11:44:05 +0000

    put back the functions

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -6,4 +6,18 @@ from base_caching import BaseCaching
 class FIFOCache(BaseCaching):
     """Defines a BasicCache class that inherits from BaseCaching"""
 
-    
+    def put(self, key: str, item: str) -> None:
+        """Adds an item to the cache if both key and item are not None"""
+        if key is not None and item is not None:  # Check both key and item
+            if len(self.cache_data) > self.MAX_ITEMS:
+                keys = list(self.cache_data.keys())
+                first_key = keys[0]
+                del self.cache_data[first_key]
+                print(f"Discard: {first_key}")
+            self.cache_data[key] = item
+
+    def get(self, key: str) -> str:
+        """Retrieves an item from the cache"""
+        if key is None:
+            return None
+        return self.cache_data.get(key)
