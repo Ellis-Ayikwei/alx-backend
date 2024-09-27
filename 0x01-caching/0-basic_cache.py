d46df422568049a2182bf69b5d1df5acd6c4d601
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-27 11:20:34 +0000

    the testpush . v1

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -6,4 +6,13 @@ from base_caching import BaseCaching
 class BasicCache(BaseCaching):
     """Defines a BasicCache class that inherits from BaseCaching"""
 
-   
+    def put(self, key: str, item: str) -> None:
+        """Adds an item to the cache"""
+        if key is not None or item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key: str) -> str:
+        """Retrieves an item from the cache"""
+        if key is None:
+            return None
+        return self.cache_data.get(key)
