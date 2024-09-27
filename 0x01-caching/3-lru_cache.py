0b3a1c45177669dfd656b79efc2077ad494136b4
Ellis-Ayikwei <ellisarmahayikwei@gmail.com>
2024-09-27 12:54:07 +0000

    tehe lru cache

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -7,8 +7,19 @@ class LRUCache(BaseCaching):
     """Defines a BasicCache class that inherits from BaseCaching"""
 
     def __init__(self):
+        super().__init__()
         self.access_order = []
 
+
+    def put(self, key: str, item: str) -> None:
+        """Adds an item to the cache if both key and item are not None"""
+        if key is not None and item is not None:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                least_recently_used_key = self.access_order.pop(0)
+                del self.cache_data[least_recently_used_key]
+                self.cache_data[key] = item
+            self.cache_data[key] = item
+            self.access_order.append(key)
     def get(self, key: str) -> str:
         """Retrieve an item from the cache"""
         if key in self.cache_data:
@@ -16,13 +27,3 @@ class LRUCache(BaseCaching):
             self.access_order.append(key)
             return self.cache_data[key]
         return None
-
-    def put(self, key: str, item: str) -> None:
-        """Adds an item to the cache if both key and item are not None"""
-        if key is not None and item is not None:  # Check both key and item
-            if len(self.cache_data) >= self.MAX_ITEMS:
-                least_recently_used_key = self.access_order.pop(0)
-                del self.cache_data[least_recently_used_key]
-
-            self.cache_data[key] = item
-            self.access_order.append(key)
